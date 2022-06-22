6500f7da840b31aaf90a7705d86c6d7d6dd55208
kantapraveen <satyasaipraveen2001@gmail.com>
2022-06-22 19:54:10 +0530

    asggggg

--- for.go
+++ for.go
@@ -5,11 +5,11 @@ import (
 )
 
 func mai() {
-	var arr = [...]int{1,2,3,5,5}
+	var arr = [...]int{1, 2, 3, 5, 5}
 	for i := 0; i <= 10; i++ {
 		fmt.Println(i)
 	}
-	for idx,value := range arr{
-		fmt.Println(idx,value)
+	for idx, value := range arr {
+		fmt.Println(idx, value)
 	}
 }
--- total.go
+++ total.go
@@ -1,5 +1,5 @@
-package main
+package mai
 
 func main() {
-	SDJFNOIS;DFNSDH;
+
 }
