b63f2d34d67decc0ec5d4f2b287e107be8de7cdf
kantapraveen <satyasaipraveen2001@gmail.com>
2022-06-29 18:07:52 +0530

    python file are formmated

--- learn.py
+++ learn.py
@@ -1,8 +1,9 @@
-print("learning")
-
-A = 1
 def Brute():
     return "Lol;jngoiwenh"
 
 def newBranchBrute():
     return "new Branch"
+
+if __name__ == "__main__":
+    A = 1
+    print("learning...")
